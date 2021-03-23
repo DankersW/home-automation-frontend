5a748cce4e227d864f89fd2ac363ee9f71d0ef67
DankersW <wouter.dankers@skynet.be>
2021-03-23 12:13:38 +0100

    fetch from api and beginning of graph

--- src/pages/Monitoring.js
+++ src/pages/Monitoring.js
@@ -1,21 +1,59 @@
 import React from "react";
 import "./Monitoring.css"
 import axios from 'axios'
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
 
-function Te() {
+
+function TempMonitor() {
+
+    const [data, setData] = React.useState([])
+
+    React.useEffect(() => {
+        const url = 'http://localhost:4000/api/temp'
+        axios.get(url)
+        .then(response => {
+            const val = response.data.message
+            console.log(val)
+            console.log(val[0].device_id)
+            const t = val.map(obj => obj);
+            console.log(t)
+            setData(t)
+
+            for(var i in val){
+                console.log(i)
+                console.log(val[i])
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+
+    }, []);
+    
     console.log("test")
-    var data
-    const dataUrl = 'https://jsonplaceholder.typicode.com/users'
-    axios.get(dataUrl).then(res => {
-            console.log(res)
-            data = res.data
-    })
+    console.log(data)
+    console.log("data above")
+    //console.log(data[0].device_id)
+    var plot_data = []
+    for(var i in data){
+        console.log(data[i])
+        var item = {x: new Date(data[i].timestamp), y: data[i].temp}
+        plot_data.push(item)
+    }
+
+    var plot_data_ = [{x: 1, y: 25.6}, {x: 2, y: 4}, {x: 3, y: 55}]
 
 
     return (
         <div>
-            <h3>Testing</h3>
-            {data}
+            <h3>Results</h3>
+            {data.length}
+            <XYPlot xType="time" width={300} height={300}>
+                <HorizontalGridLines />
+                <LineSeries data={plot_data}/>
+                <XAxis />
+                <YAxis />
+            </XYPlot>
         </div>
     )
 }
@@ -26,7 +64,7 @@ function Monitoring() {
             <h1>Monitoring</h1>
 
             <p>Graph data goes here</p>
-            <Te></Te>
+            <TempMonitor></TempMonitor>
         </div>
     );
 }
