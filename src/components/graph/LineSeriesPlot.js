69141fddc722f99a8265e22aaf4d537f38239049
DankersW <wouter.dankers@skynet.be>
2021-03-24 09:28:48 +0100

    LineSeriesPlotter made more generic using function props

--- src/components/graph/LineSeriesPlot.js
+++ src/components/graph/LineSeriesPlot.js
@@ -2,19 +2,18 @@ import React from 'react'
 import axios from 'axios'
 import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
 import AutoSizer from 'react-virtualized-auto-sizer'
-import "./TemperaturePlotter.css"
+import "./LineSeriesPlot.css"
 
-function TemperaturePlotter() {
+function LineSeriesPlot({title, url, xAxis, yAxis}) {
     const [plotData, setPlotData] = React.useState([])
 
     React.useEffect(() => {
-        const url = 'http://localhost:4000/api/temp'
         axios.get(url)
         .then(response => {
             const messageData = response.data.message
             var points = []
             for (var i in messageData){
-                var plotPoint = {x: new Date(messageData[i].timestamp), y: messageData[i].temp}
+                var plotPoint = {x: new Date(messageData[i][xAxis]), y: messageData[i][yAxis]}
                 points.push(plotPoint)
             }
             setPlotData(points)
@@ -22,12 +21,12 @@ function TemperaturePlotter() {
         .catch((error) => {
             console.log(error)
         })
-    }, []);
+    }, [url, xAxis, yAxis]);
 
     return (
         <div className="plot-group">
             <div className="plot-title">
-                <h3>Temperature</h3>
+                <h3>{title}</h3>
             </div>
             <AutoSizer>
             {({ width }) => (
@@ -45,4 +44,4 @@ function TemperaturePlotter() {
     )
 }
 
-export default TemperaturePlotter
+export default LineSeriesPlot
--- src/pages/Monitoring.js
+++ src/pages/Monitoring.js
@@ -1,17 +1,21 @@
 import React from "react";
 import "./Monitoring.css"
-import TemperaturePlotter from "../components/graph/TemperaturePlotter"
+import LineSeriesPlot from "../components/graph/LineSeriesPlot"
 
-//
+
 function Monitoring() {
     return (
         <div className="monitoring">
             <h1 className="title">Monitoring</h1>
             <div className="monitor-grid">
-                <div class="tile-wide"> <TemperaturePlotter></TemperaturePlotter> </div>
+                <div class="tile-wide"> 
+                    <LineSeriesPlot title="Temperature" url="http://localhost:4000/api/temp" xAxis="timestamp" yAxis="temp"/>
+                </div>
                 <div class="tile ">2</div>
                 <div class="tile ">3</div>
-                <div class="tile tile-wide">4</div>
+                <div class="tile-wide"> 
+                    <LineSeriesPlot title="Humidity" url="http://localhost:4000/api/temp" xAxis="timestamp" yAxis="humi"/>
+                </div>
                 <div class="tile ">5</div>
                 <div class="tile tile-tall">6</div>
                 <div class="tile ">7</div>
