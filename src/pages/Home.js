f5973378da0444e5aa1629196da7281473d68fe1
DankersW <wouter.dankers@skynet.be>
2021-04-08 21:04:14 +0200

    Cleanup links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,25 +2,33 @@ import React from "react";
 import "./Home.css"
 
 function Home() {
-
     var config = require('../config');
 
     var links = [
         {
             title: "DB viewer",
             url: config.mongo_express
+        },
+        {
+            title: "Backend API options",
+            url: config.api_list 
         }
     ]
 
     return (
-        <div className="home-container">
+        <div className="home-flex-container">
             <h1>Home</h1>
-            {links.map((item) => {
-                return (
-                    <a href={item.url} target="_blank" rel="noopener noreferrer">{item.title}</a>
-                );
-            })}
-            
+            <div className="home-links-container">
+                <ul>
+                    {links.map((item, index) => {
+                        return (
+                            <li key={"home_li_" + index}>
+                                <a href={item.url} target="_blank" rel="noopener noreferrer">{item.title}</a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
         </div>
     );
 }
