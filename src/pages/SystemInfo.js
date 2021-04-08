d558ac12b0a292e03166a20b2db2b765ec52576c
DankersW <wouter.dankers@skynet.be>
2021-04-08 21:15:50 +0200

    Note about ip address added

--- src/pages/SystemInfo.js
+++ src/pages/SystemInfo.js
@@ -5,7 +5,7 @@ function SystemInfo() {
     return (
         <div className="SystemInfo">
             <h1>System Info</h1>
-            <p>docker info, which versions are running, status of each docker container, etc.</p>
+            <p>docker info, which versions are running, status of each docker container, IP address host, etc.</p>
         </div>
     );
 }
