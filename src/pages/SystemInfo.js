ce0906e1fcdf5bc5c92b0e1cee3a8cab6368ddee
DankersW <wouter.dankers@skynet.be>
2021-04-08 22:02:15 +0200

    Skeleton dense table

--- src/pages/SystemInfo.js
+++ src/pages/SystemInfo.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./SystemInfo.css"
+import DenseTable from "./../components/table/Table"
 
 function SystemInfo() {
     return (
-        <div className="SystemInfo">
+        <div className="system-info-container">
             <h1>System Info</h1>
             <p>docker info, which versions are running, status of each docker container, IP address host, etc.</p>
+            <DenseTable></DenseTable>
         </div>
     );
 }
