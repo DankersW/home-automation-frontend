9c12d1576b5a7c6bcb7a6739d3b7b69620539fc7
DankersW <wouter.dankers@skynet.be>
2021-04-08 20:55:23 +0200

    Fixing className bug

--- src/pages/Monitoring.js
+++ src/pages/Monitoring.js
@@ -11,22 +11,22 @@ function Monitoring() {
         <div className="monitoring">
             <h1 className="title">Monitoring</h1>
             <div className="monitor-grid">
-                <div class="tile-wide"> 
+                <div className="tile-wide"> 
                     <LineSeriesPlot title="Temperature" url={tempHumiUrl} xAxis="timestamp" yAxis="temp"/>
                 </div>
-                <div class="tile ">2</div>
-                <div class="tile ">3</div>
-                <div class="tile-wide"> 
+                <div className="tile ">2</div>
+                <div className="tile ">3</div>
+                <div className="tile-wide"> 
                     <LineSeriesPlot title="Humidity" url={tempHumiUrl} xAxis="timestamp" yAxis="humi"/>
                 </div>
-                <div class="tile ">5</div>
-                <div class="tile tile-tall">6</div>
-                <div class="tile ">7</div>
-                <div class="tile ">8</div>
-                <div class="tile ">9</div>
-                <div class="tile ">10</div>
-                <div class="tile tile-wide tile-tall">11</div>
-                <div class="tile ">12</div>
+                <div className="tile ">5</div>
+                <div className="tile tile-tall">6</div>
+                <div className="tile ">7</div>
+                <div className="tile ">8</div>
+                <div className="tile ">9</div>
+                <div className="tile ">10</div>
+                <div className="tile tile-wide tile-tall">11</div>
+                <div className="tile ">12</div>
             </div>
         </div>
     );
