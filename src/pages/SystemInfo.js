5c697ff132567a77d38f0f852923bb326475bba1
DankersW <wouter.dankers@skynet.be>
2021-04-13 23:15:04 +0200

    Added call to api backend

--- src/pages/SystemInfo.js
+++ src/pages/SystemInfo.js
@@ -1,14 +1,30 @@
 import React from "react";
 import "./SystemInfo.css"
 import DenseTable from "./../components/table/Table"
+import axios from 'axios'
 
-var docker_data_hearders = ["version", "alias", "uptime", "status", "port"]
-var docker_data = [{name:'front-end', version: "v1.1.3", alias: "frontend", uptime: "546 seconds", status: "running", port: "80"},
-{name:'backend', version: "v1.0.1", alias: "backend", uptime: "30 seconds", status: "running", port: "5000"}]
+//var docker_data_hearders = ["version", "alias", "uptime", "status", "port"]
+//var docker_data = [{name:'front-end', version: "v1.1.3", alias: "frontend", uptime: "546 seconds", status: "running", port: "80"},
+//{name:'backend', version: "v1.0.1", alias: "backend", uptime: "30 seconds", status: "running", port: "5000"}]
 
 
 
 function SystemInfo() {
+
+    var config = require('../config');
+    const url_docker_data = config.docker_data
+    
+    React.useEffect(() => {
+        axios.get(url_docker_data)
+        .then(response => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    });
+    var docker_data_hearders = ["version", "alias", "uptime", "status", "port"]
+    var docker_data = []
 
     return (
         <div className="system-info-container">
@@ -22,7 +38,6 @@ function SystemInfo() {
                 <h2>Host info (IP address)</h2>
                 <DenseTable table_headers={docker_data_hearders} table_data={docker_data}></DenseTable>
             </div>
-            
         </div>
     );
 }
