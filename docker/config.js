dfe4b7c6243a12b48c5e56e73fb615cbb89027d4
DankersW <wouter.dankers@skynet.be>
2021-04-13 22:36:46 +0200

    adding new docker info url

--- docker/config.js
+++ docker/config.js
@@ -6,6 +6,7 @@ const backend = docker_host + ":4000"
 config.docker_host = docker_host
 config.temp_humi_url= backend + "/api/temp"
 config.api_list = backend + "/api/"
+config.docker_data = backend + "/api/docker_info"
 config.mongo_express = docker_host + ":8081" 
 
 module.exports = config;
--- src/config.js
+++ src/config.js
@@ -10,6 +10,7 @@ config.docker_host = docker_host
 
 config.temp_humi_url = backend + "/api/temp"
 config.api_list = backend + "/api/"
+config.docker_data = backend + "/api/docker_info"
 config.mongo_express = docker_host + ":8081" 
 
 module.exports = config;
