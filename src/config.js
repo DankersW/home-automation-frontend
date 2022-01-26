0b668f0d641639bac4a1b3a10703075602596ee2
DankersW <wouter.dankers@skynet.be>
2022-01-26 18:16:50 +0100

    docs: update how to run

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ config.docker_host = docker_host
 // TODO: this need to change, also look at prod config
 config.temp_humi_url = backend + "/api/temp"
 config.api_list = backend + "/api/"
-config.docker_data = backend + "/api/docker_info"
+config.docker_data = backend + "/api/docker_info"   
 config.mongo_express = docker_host + ":8081" 
 
 module.exports = config;
